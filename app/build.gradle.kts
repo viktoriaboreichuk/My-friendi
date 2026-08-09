194740373af29a89cb2873e05043064448bf0c2b
viktoriaboreichuk <viktoriaboreichuk@gmail.com>
2026-08-09 19:45:29 +0300

    Bump Lunari to 0.2.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.vega.yakor"
         minSdk = 26
         targetSdk = 36
-        versionCode = 1
-        versionName = "0.1.0"
+        versionCode = 2
+        versionName = "0.2.0"
     }
 
     buildFeatures {
